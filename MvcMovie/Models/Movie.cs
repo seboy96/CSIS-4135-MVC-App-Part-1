fcf59555dcf0dd55dfdbdcc61e86fd7ee458f933
Sean Boyle <boyles8@go.stockton.edu>
2018-10-17 23:36:35 -0400

    CSIS 4135 Lab 5 final

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required (ErrorMessage = "Please enter a title!")]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
+        [DataType(DataType.Date)]
+        [Required (ErrorMessage = "Please enter a release date!")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required (ErrorMessage = "Please enter a genre!")]
+        public string Genre { get; set; }
+
+        [Range(1, Double.MaxValue)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [Required (ErrorMessage = "Please enter a price!")]
+        public decimal Price { get; set; }
+
+        [Required (ErrorMessage = "Please enter a rating!")]
+        public string Rating { get; set; }
+    }
+}
